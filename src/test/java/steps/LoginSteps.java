80dd56494390f8cdd32d47971dcea71b87356dca
Fahim Ahmed <apex.fahimahmed@gmail.com>
2026-06-28 15:53:06 -0400

    Add Cucumber TestNG runner and enable initial BDD execution flow

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -4,12 +4,16 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import pages.LoginPage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginSteps {
 
+    WebDriver driver = new ChromeDriver();
+
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        // TODO: navigate to login page
+        driver.get("https://opensource-demo.orangehrmlive.com/");
     }
 
     @When("I login with valid admin credentials")
