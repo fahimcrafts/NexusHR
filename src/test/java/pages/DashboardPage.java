8f091eeae5bb7899088d1401b02f056dad007057
Fahim Ahmed <apex.fahimahmed@gmail.com>
2026-06-30 22:05:16 -0400

    refactor: centralize page synchronization using BasePage wait helpers

--- src/test/java/pages/DashboardPage.java
+++ src/test/java/pages/DashboardPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
@@ -8,17 +9,14 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class DashboardPage {
-
-    private final WebDriver driver;
-
+public class DashboardPage extends BasePage {
     private final By dashboardHeader = By.xpath("//h6[normalize-space()='Dashboard']");
 
     public DashboardPage(WebDriver driver) {
-        this.driver = driver;
+        super(driver);
     }
 
     public boolean isDashboardDisplayed() {
-        return new WebDriverWait(driver, Duration.ofSeconds(10)).until(ExpectedConditions.visibilityOfElementLocated(dashboardHeader)).isDisplayed();
+        return waitForVisibility(dashboardHeader).isDisplayed();
     }
 }
