661f2e7be8558c3911f6f75db1398250eea5e541
Fahim Ahmed <apex.fahimahmed@gmail.com>
2026-09-06 11:24:56 -0400

    feat: enable parallel Cucumber scenario execution

--- src/test/java/runner/SuiteRunner.java
+++ src/test/java/runner/SuiteRunner.java
@@ -2,6 +2,7 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
         features = "src/test/resources/features",
@@ -16,4 +17,9 @@ import io.cucumber.testng.CucumberOptions;
 
 public class SuiteRunner extends AbstractTestNGCucumberTests {
 
+        @Override
+        @DataProvider(parallel = true)
+        public Object[][] scenarios() {
+                return super.scenarios();
+        }
 }
