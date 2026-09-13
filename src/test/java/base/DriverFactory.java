cbd75f5f39ae7ba0eb1c68042802be286dc8b185
Fahim Ahmed <apex.fahimahmed@gmail.com>
2026-09-13 12:35:11 -0400

    Run Chrome headless with deterministic viewport

--- src/test/java/base/DriverFactory.java
+++ src/test/java/base/DriverFactory.java
@@ -1,10 +1,9 @@
 package base;
 
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import utils.ConfigReader;
 
 public class DriverFactory {
@@ -22,7 +21,11 @@ public class DriverFactory {
 
         switch(browser.toLowerCase()) {
             case "chrome":
-                driver.set(new ChromeDriver());
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--headless=new");
+                options.addArguments("--window-size=1920,1080");
+
+                driver.set(new ChromeDriver(options));
                 break;
 
             case "firefox":
@@ -34,15 +37,6 @@ public class DriverFactory {
                         "Unsupported browser: " + browser
                 );
         }
-
-        driver.get().manage().window().setSize(new Dimension(1920, 1080));
-
-        System.out.println("Jenkins Browser Window: "
-        + driver.get().manage().window().getSize());
-        System.out.println("Jenkins Viewport: "
-                + ((JavascriptExecutor) driver.get())
-                .executeScript("return window.innerWidth + 'x' + window.innerHeight;"
-                ));
 
         return driver.get();
     }
